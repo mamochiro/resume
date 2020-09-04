de0501eca9b4b7f0f6a4dfde874eb3c9237dd309
sarawut nawawisitkul <mamochiro11@gmail.com>
2020-09-04 16:14:24 +0700

    fix: config

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,6 +70,9 @@ export default {
      */
     publicPath: '/assets/', // for build file ext : git hub page
     extend(config, ctx) {}, // eslint-disable-line
+    babel: {
+      compact: true,
+    },
   },
 
   // for github page
